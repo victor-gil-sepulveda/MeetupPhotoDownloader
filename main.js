6806a0855913a8d4891ed62a5786a9c878139134
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-12-23 14:46:21 +0100

    Event population (IX)

--- main.js
+++ main.js
@@ -64,12 +64,18 @@ require(["js/meetup.tools.js", "js/url.tools.js"], function(meetup, url_tools) {
 
 
                 $("#groups_table").empty();
+                var i = 0;
                 for(var group_id in g_by_events){
                     $("#groups_table").append(
                         "<div class='group_name' data-groupid='" + group_id+"'><p>"+
                         g_by_events[group_id][0].group.name+
                         "</p></div>");
 
+                    if (i!= (g_by_events.length - 1)){
+                        $("#groups_table").append("<hr>");
+                    }
+
+                    i = i+1;
                 }
                 // Then add the callback for clicks
                 $( ".group_name" ).click(function(){
@@ -87,6 +93,9 @@ require(["js/meetup.tools.js", "js/url.tools.js"], function(meetup, url_tools) {
                             event_list[i].group.urlname+"'><p>"+
                             g_by_events[group_id][i].event.name+
                             "</p></div>");
+                        if (i!= (event_list.length - 1)){
+                            $("#events_table").append("<hr>");
+                        }
                     }
                 });
             })
