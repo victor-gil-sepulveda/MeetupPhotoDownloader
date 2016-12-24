fe2d4e2f72dfb855d7049f5a8f8810b350339a09
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-12-24 21:21:05 +0100

    Experimenting with progress bar

--- main.js
+++ main.js
@@ -219,6 +219,38 @@ require(["js/meetup.tools.js", "js/url.tools.js"], function(meetup, url_tools) {
             });
     }
 
+    function do_progress(){
+        var bar = new ProgressBar.Line("progress_bar_container", {
+          strokeWidth: 4,
+          easing: 'easeInOut',
+          duration: 1400,
+          color: '#FFEA82',
+          trailColor: '#eee',
+          trailWidth: 1,
+          svgStyle: {width: '100%', height: '100%'},
+          text: {
+            style: {
+              // Text color.
+              // Default: same as stroke color (options.color)
+              color: '#999',
+              position: 'absolute',
+              right: '0',
+              top: '30px',
+              padding: 0,
+              margin: 0,
+              transform: null
+            },
+            autoStyleContainer: false
+          },
+          from: {color: '#FFEA82'},
+          to: {color: '#ED6A5A'},
+          step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %');
+          }
+        });
+
+    }
+
     $(document).ready(function(){
         console.log("Main loaded")
 
